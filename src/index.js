2b853c40cae358c2e4729671692eaffb772d9a25
Vitor Boccio <vitorboccio@gmail.com>
2019-02-06 21:50:53 +1100

    initial router config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App}></Route>
+      <Route path="/login" component={Login}></Route>
+      <Route path="/register" component={Register}></Route>
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
