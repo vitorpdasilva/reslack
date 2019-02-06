0574d33ac56a48b76a67ee54acbfe48aa41ee3e1
Vitor Boccio <vitorboccio@gmail.com>
2019-02-06 22:30:52 +1100

    layout for register page

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { Form, Grid, Segment, Button, Header, Icon, Message } from 'semantic-ui-react';
 
 class Register extends Component {
+  state = {};
+  
+  handleChange = () => {
+
+  }
   render() {
     return (
-      <div>
-        Register
-      </div>
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for Reslack
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input fluid name="username" type="text" icon="user" iconPosition="left" placeholder="username" onChange={this.handleChange} />
+              <Form.Input fluid name="email" type="email" icon="mail" iconPosition="left" placeholder="email" onChange={this.handleChange} />
+              <Form.Input fluid name="password" type="password" icon="lock" iconPosition="left" placeholder="password" onChange={this.handleChange} />
+              <Form.Input fluid name="passwordConfirmation" type="password" icon="repeat" iconPosition="left" placeholder="password confirmation" onChange={this.handleChange} />
+              <Button color="orange" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>Already a user? <Link to="/login">Login</Link></Message>
+        </Grid.Column>
+
+      </Grid>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
