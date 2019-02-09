ed9b998c49468b807a81803da825acd48ea9d3d4
Vitor Boccio <vitorboccio@gmail.com>
2019-02-09 15:48:52 +1100

    creating global store

--- src/index.js
+++ src/index.js
@@ -7,12 +7,20 @@ import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 import firebase from 'firebase';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends Component {
   
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if(user) {
+        this.props.setUser(user);
         this.props.history.push('/');
       }
     })
@@ -29,10 +37,12 @@ class Root extends Component {
   }
 };
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-<Router>
-  <RootWithAuth />
-</Router>, document.getElementById('root'));
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
