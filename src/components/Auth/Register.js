22d62bd680c31e06f08acd565bef0bf15d95e075
Vitor Boccio <vitorboccio@gmail.com>
2019-02-09 14:44:26 +1100

    improving form validation4

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -10,6 +10,7 @@ class Register extends Component {
     password: '',
     passwordConfirmation: '',
     errors: [],
+    loading: false,
   };
   
   handleChange = event => {
@@ -25,7 +26,7 @@ class Register extends Component {
       error = { message: 'Fill all fields' };
       this.setState({ errors: errors.concat(error) });
       return false;
-    } else if (this.isPasswordValid(this.state)) {
+    } else if (!this.isPasswordValid(this.state)) {
         error = { message: 'Password isnt valid'};
         this.setState({ errors: errors.concat(error) });
         return false;
@@ -35,36 +36,39 @@ class Register extends Component {
   }
 
   isFormEmpty = ({username, email, password, passwordConfirmation}) => {
-    return !username.length || !email.length || password.length || passwordConfirmation.length; 
+    return !username.length || !email.length || !password.length || !passwordConfirmation.length; 
   }
 
   isPasswordValid = ({ password, passwordConfirmation }) => {
     if (password.length < 6 || passwordConfirmation.length < 6) {
-      return false
+      return false;
     } else if (password !== passwordConfirmation) {
-      return false
+      return false;
     } else {
       return true;
     }
   }
 
   submitHandler = event => {
+    event.preventDefault();
+    const { email, password, errors } = this.state;
     if (this.isFormValid()) {
-      const { email, password } = this.state;
-      event.preventDefault();
+      this.setState({ errors: [], loading: true });
       firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
       .then(createdUser => {
         console.log(createdUser);
+        this.setState({ loading: false })
       }).catch(err => {
         console.log(err);
+        this.setState({ errors: errors.concat(err), loading: false })
       })
     }
   }
 
   render() {
-    const { username, email, password, passwordConfirmation, errors } = this.state;
+    const { username, email, password, passwordConfirmation, errors, loading } = this.state;
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -78,7 +82,7 @@ class Register extends Component {
               <Form.Input fluid name="email" value={email} type="email" icon="mail" iconPosition="left" placeholder="email" onChange={this.handleChange} />
               <Form.Input fluid name="password" value={password} type="password" icon="lock" iconPosition="left" placeholder="password" onChange={this.handleChange} />
               <Form.Input fluid name="passwordConfirmation" value={passwordConfirmation} type="password" icon="repeat" iconPosition="left" placeholder="password confirmation" onChange={this.handleChange} />
-              <Button color="orange" fluid size="large">
+              <Button color="orange" disabled={loading} fluid size="large" className={loading ? 'loading' : null}>
                 Submit
               </Button>
             </Segment>
