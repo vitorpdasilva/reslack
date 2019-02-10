bc3842260e114b9cbb379c8de62c658e4f0be655
Vitor Boccio <vitorboccio@gmail.com>
2019-02-10 14:10:38 +1100

    including message on message collection

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import firebase from '../../firebase';
 import { Segment, Comment } from 'semantic-ui-react';
 import MessagesHeader from './MessagesHeader';
 import MessagesForm from './MessagesForm'
 
 export default class Messages extends Component {
+  
+  state = {
+    channel: this.props.currentChannel,
+    messagesRef: firebase.database().ref('messages'),
+    user: this.props.currentUser,
+  }
+
   render() {
+    const { messagesRef, channel, user } = this.state;
     return (
       <React.Fragment>
         <MessagesHeader />
         <Segment>
           <Comment.Group className="messages" />
         </Segment>
-        <MessagesForm />
+        <MessagesForm currentUser={user}  messagesRef={messagesRef} currentChannel={channel} />
       </React.Fragment>
     )
   }
--- src/components/Messages/MessagesForm.js
+++ src/components/Messages/MessagesForm.js
@@ -1,8 +1,60 @@
 import React, { Component } from 'react';
+import firebase from '../../firebase';
 import { Segment, Button, Input} from 'semantic-ui-react';
 
 class MessagesForm extends Component {
+  
+  state = {
+    message: '',
+    loading: false,
+    channel: this.props.currentChannel,
+    user: this.props.currentUser,
+    errors: [],
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  createMessage = () => {
+    const { user } = this.state;
+    const message = {
+      content: this.state.message,
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      user: {
+        id: user.uid,
+        name: user.displayName,
+        avatar: user.photoURL,
+      }
+    }
+    return message;
+  }
+
+  sendMessage = () => {
+    const { messagesRef } = this.props;
+    const { message, channel, errors } = this.state;
+
+    if(message) {
+      this.setState({ loading: true });
+      messagesRef
+        .child(channel.id)
+        .push()
+        .set(this.createMessage())
+        .then(() => {
+          this.setState({ loading: false, message: '', errors: [], })
+        }).catch(err => {
+          console.log(err);
+          this.setState({ errors: errors.concat(err) })
+        })
+    } else {
+      this.setState({  
+        errors: errors.concat({ message: 'Add a message...' })
+      });
+    }
+  }
+
   render() {
+    const { errors } = this.state;
     return (
       <Segment className="message__form">
         <Input 
@@ -12,9 +64,13 @@ class MessagesForm extends Component {
           label={<Button icon={"add"} />}
           labelPosition="left"
           placeholder="write your message"
+          onChange={this.handleChange}
+          className={errors.some(error =>
+            error.message.includes('message')) ? 'error' : null
+          }
         />
         <Button.Group icon widths="2">
-          <Button color="orange" content="add Reply" labelPosition="left" icon="edit" />
+          <Button color="orange" content="add Reply" onClick={this.sendMessage} labelPosition="left" icon="edit" />
           <Button color="teal" content="upload media" labelPosition="right" icon="cloud upload" />
         </Button.Group>
       </Segment>
